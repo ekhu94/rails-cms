e35e773b37574c96a62c44a618edb185d306948e
Erik Huang <ekhu94@gmail.com>
2021-06-07 17:03:59 -0700

    creates controller actions for Pages and Subjects, adds resourceful routes including delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,17 @@ Rails.application.routes.draw do
   get 'demo/index'
   get 'demo/about'
   get 'demo/contact'
+
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
+
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210607225440_create_pages.rb
@@ -0,0 +1,14 @@
+class CreatePages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pages do |t|
+      t.string :name
+      t.string :permalink
+      t.integer :position
+      t.boolean :visible
+      t.text :content
+      t.references :subject, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
